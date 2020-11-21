80ba041c18220d72965563474106f8605a8e92db
AmjadMoqade98 <amjadmoqade98@gmail.com>
2020-11-22 01:23:22 +0200

    coloring the console output

--- src/project/utils/ColoredText.java
+++ src/project/utils/ColoredText.java
@@ -1,4 +1,15 @@
 package project.utils;
 
 public class ColoredText {
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String RED_TEXT = "\u001B[31m";
+    public static final String GREEN_TEXT = "\u001B[32m";
+
+    public static String ToRed(String text) {
+        return RED_TEXT + text + ANSI_RESET ;
+    }
+
+    public static String ToGreen(String text) {
+        return GREEN_TEXT + text + ANSI_RESET ;
+    }
 }
